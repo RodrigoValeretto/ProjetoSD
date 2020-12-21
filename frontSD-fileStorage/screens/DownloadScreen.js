05630e315d31ce8e75988ee6f97b943aae919ac6
rodrigovaleretto <rodrigovaleretto@usp.br>
2020-12-21 00:38:25 -0300

    Download de file iniciado

--- frontSD-fileStorage/screens/DownloadScreen.js
+++ frontSD-fileStorage/screens/DownloadScreen.js
@@ -4,6 +4,10 @@ import { Container, StartContainer, ItemText, ItemList, IconButton } from "../St
 import { useNavigation, useRoute } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import axios from "axios";
+import * as FileSystem from "expo-file-system";
+import Permissions from "expo-permissions";
+import * as Sharing from "expo-sharing";
+import { Buffer } from "buffer";
 
 class DownloadScreenClasse extends React.Component {
   state: {
@@ -18,11 +22,11 @@ class DownloadScreenClasse extends React.Component {
   }
 
   async componentDidMount() {
-    let downloads = await this.getEnquetes();
+    let downloads = await this.getDownloads();
     this.setState({ downloadsList: downloads });
   }
 
-  async getEnquetes() {
+  async getDownloads() {
     let resultado = null;
 
     await axios
@@ -38,21 +42,44 @@ class DownloadScreenClasse extends React.Component {
     return resultado;
   }
 
-  async downloadFile(id) {
+  async downloadFile(id, name) {
     let resultado = null;
+    console.log("id: ", id);
 
-    await axios
-      .get("https://storagefiles.herokuapp.com/downloadFile/" + id)
-      .then((res) => {
-        resultado = res.data;
-        console.log(res.data);
+    const headers = {
+      "Content-Type": "text/html",
+      Accept: "*/*",
+      "Accept-Encoding": "gzip, deflate, br",
+    };
+
+    // await axios
+    //   .get(`https://storagefiles.herokuapp.com/downloadFile/${id}`, { headers, responseType: "arraybuffer" })
+    //   .then(async (res) => {
+    //     resultado = Buffer.from(res.data, "base64");
+    //     resultado = resultado.toString("base64");
+
+    //     const fileUri = FileSystem.documentDirectory;
+    //     console.log(fileUri);
+    //     await FileSystem.writeAsStringAsync(fileUri, resultado, { encoding: FileSystem.EncodingType.Base64 });
+    //     await Sharing.shareAsync(fileUri + `${encodeURI(name)}`);
+    //     //resultado = res.data;
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     resultado = null;
+    //   });
+
+    FileSystem.downloadAsync(`https://storagefiles.herokuapp.com/downloadFile/${id}`, FileSystem.documentDirectory)
+      .then(async ({ uri }) => {
+        console.log(uri);
+        // console.log(uri + "/" + name);
+        // const dir = uri.slice(0, uri.indexOf("%2540"));
+        // console.log(dir + name);
+        await Sharing.shareAsync(uri);
       })
       .catch((err) => {
         console.log(err);
-        resultado = null;
       });
-
-    return resultado;
   }
 
   render() {
@@ -73,7 +100,7 @@ class DownloadScreenClasse extends React.Component {
                   <Text style={{ color: "black", fontSize: 18, textAlign: "left" }}>Tamanho: {obj.item.size} (Bytes)</Text>
                   <Text style={{ color: "black", fontSize: 15, textAlign: "left" }}> Data de upload: {date.getDate() + "/" + date.getMonth() + "/" + date.getYear()}</Text>
                 </View>
-                <IconButton onPress={() => this.downloadFile(obj.item._id)}>
+                <IconButton onPress={() => this.downloadFile(obj.item._id, obj.item.name)}>
                   {/* <ButtonText onPress={() => navigation.navigate("EnqueteScreen", { userId: userId, enqueteId: obj.item.id, tituloEnquete: obj.item.titulo })}>Download</ButtonText> */}
                   <Icon name="download" size={25} color="white" />
                 </IconButton>
